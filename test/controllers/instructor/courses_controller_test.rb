0c3d47406ff59614c5911981bfd29fc6156b7478
John Towery <kbaries@gmail.com>
2015-12-16 06:13:40 +0000

    F:L12 End of lesson 12 but working on test

--- test/controllers/instructor/courses_controller_test.rb
+++ test/controllers/instructor/courses_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class Instructor::CoursesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "course show page" do
+    course = Course.create(:title=> 'Count', :description => 'How to go from 1 to 10', :cost => '1.00')
+    get :show, :id => course.id
+    assert_response :success
+  end
+
+  test "quote show page, not found" do
+		get :show, :id => 'OMG'
+    assert_response :not_found
+  end
 end
